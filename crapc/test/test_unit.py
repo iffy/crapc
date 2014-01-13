827bf2fcae7b5bf46f1ebdab0cfa9e10b173dcaf
Matt Haggard <haggardii@gmail.com>
2014-01-13 16:43:06 -0700

    Make descriptor usable

--- crapc/test/test_unit.py
+++ crapc/test/test_unit.py
@@ -1,12 +1,12 @@
 from twisted.trial.unittest import TestCase
 from zope.interface.verify import verifyObject
 
-from mock import create_autospec
+from mock import create_autospec, MagicMock
 
 from crapc.interface import ISystem
 from crapc._request import Request
 from crapc.error import MethodNotFound
-from crapc.unit import RPCSystem
+from crapc.unit import RPCSystem, RPC
 
 
 class RPCSystemTest(TestCase):
@@ -72,3 +72,101 @@ class RPCSystemTest(TestCase):
         self.assertEqual(foo_req.method, 'bar')
         self.assertEqual(foo.runProcedure.call_count, 1)
         self.assertEqual(r, 'hey', "Should")
+
+
+
+class RPCTest(TestCase):
+
+
+    def test_ISystem(self):
+        class Foo(object):
+            rpc = RPC()
+
+        foo = Foo()
+        verifyObject(ISystem, foo.rpc)
+
+
+    def test_sameInstance(self):
+        """
+        You should get the same instance each time you access the descriptor
+        """
+        class Foo(object):
+            rpc = RPC()
+
+        foo = Foo()
+        self.assertEqual(foo.rpc, foo.rpc)
+
+
+    def test_subSystem(self):
+        """
+        You can register subSystem fetchers by decorating them.
+        """
+        called = []
+        ret = MagicMock()
+        ret.runProcedure.return_value = 'ret val'
+
+        class Foo(object):
+            rpc = RPC()
+            @rpc.subSystem('foo')
+            def foo_system(self, request):
+                called.append(request)
+                return ret
+
+        foo = Foo()
+        req = Request('foo.bar')
+        
+        result = foo.rpc.runProcedure(req)
+        
+        self.assertEqual(len(called), 1, "Should have called foo_system")
+        child_req = called[0]
+        self.assertEqual(child_req.method, 'bar')
+        self.assertIdentical(child_req.context, req.context)
+
+        self.assertEqual(result, 'ret val')
+
+
+    def test_default(self):
+        """
+        You can register a handler to generate a system that will handle all
+        non-prefixed requests.
+        """
+        called = []
+        ret = MagicMock()
+        ret.runProcedure.return_value = 'ret val'
+
+        class Foo(object):
+            rpc = RPC()
+            @rpc.default
+            def default(self, request):
+                called.append(request)
+                return ret
+
+        foo = Foo()
+        req = Request('foo.bar.baz')
+
+        result = foo.rpc.runProcedure(req)
+
+        self.assertEqual(len(called), 1, "Should have called default")
+        child_req = called[0]
+        self.assertEqual(child_req.method, 'foo.bar.baz', "Should NOT have "
+                         "removed any prefix segments")
+        self.assertIdentical(child_req.context, req.context)
+
+        self.assertEqual(result, 'ret val')
+
+
+    def test_noDefault_noSubsystem(self):
+        """
+        If there is no subsystem and no default factory, then raise
+        MethodNotFound
+        """
+        class Foo(object):
+            rpc = RPC()
+
+        foo = Foo()
+        self.assertRaises(MethodNotFound, foo.rpc.runProcedure,
+                          Request('foo'))
+        self.assertRaises(MethodNotFound, foo.rpc.runProcedure,
+                          Request('foo.bar'))
+
+
--- crapc/unit.py
+++ crapc/unit.py
@@ -1,5 +1,6 @@
 from zope.interface import implements
 
+from weakref import WeakKeyDictionary
 
 from crapc.error import MethodNotFound
 from crapc.interface import ISystem
@@ -39,3 +40,70 @@ class RPCSystem(object):
 
     def addSystem(self, name, system):
         self._systems[name] = system
+
+
+
+class _BoundRPC(object):
+
+    implements(ISystem)
+
+    def __init__(self, instance, descriptor):
+        self.instance = instance
+        self.descriptor = descriptor
+
+    def runProcedure(self, request):
+        system_factory = None
+        if '.' in request.method:
+            system_name, rest = request.method.split('.', 1)
+            try:
+                system_factory = self.descriptor._systems[system_name]
+            except KeyError:
+                pass
+
+        if system_factory:
+            request = request.child()
+        else:
+            system_factory = self.descriptor._default_system
+
+        if not system_factory:
+            raise MethodNotFound(request.method)
+
+        system = system_factory(self.instance, request)
+        return system.runProcedure(request)
+
+
+
+class RPC(object):
+    """
+    Descriptor interface for RPC routing.
+    """
+
+
+    def __init__(self):
+        self._bound_instances = WeakKeyDictionary()
+        self._systems = {}
+        self._default_system = None
+
+
+    def __get__(self, obj, type=None):
+        bound_rpc = self._bound_instances.get(obj)
+        if bound_rpc is None:
+            bound_rpc = _BoundRPC(obj, self)
+            self._bound_instances[obj] = bound_rpc
+        return bound_rpc
+
+
+    def subSystem(self, system_name):
+        def deco(f):
+            self._systems[system_name] = f
+            return f
+        return deco
+
+
+    def default(self, f):
+        self._default_system = f
+        return f
+
+
+
+
